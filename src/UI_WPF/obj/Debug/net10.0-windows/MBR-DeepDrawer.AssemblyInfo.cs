f3de3f739bcb8b2ba3df97dc5aca4df86d8c98ad
everyonelovespepsicola <40620033+everyonelovespepsicola@users.noreply.github.com>
2026-05-18 15:12:55 -0700

    GNU GENERAL PUBLIC LICENSE

--- src/UI_WPF/obj/Debug/net10.0-windows/MBR-DeepDrawer.AssemblyInfo.cs
+++ src/UI_WPF/obj/Debug/net10.0-windows/MBR-DeepDrawer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MBR-DeepDrawer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be044ecf480be543cb8d20d6957c20a0976ca69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+411fb5d3cf4ecd3dd5e7758b123e62a6fc896357")]
 [assembly: System.Reflection.AssemblyProductAttribute("MBR-DeepDrawer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MBR-DeepDrawer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
